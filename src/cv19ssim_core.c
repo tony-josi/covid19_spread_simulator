7439be5a960da8055203c06e0515a75757bf7243
Tony Josi <tonyjosinew@gmail.com>
2020-05-07 23:10:07 +0530

    sudo for basic flow

---
+++ src/cv19ssim_core.c
@@ -0,0 +1,24 @@
+// sudo code
+
+// main driver
+/*
+
+while(1)
+
+    new deaths
+
+    covid deaths
+
+    new births
+
+    movement
+
+    new infections
+
+    diagnosis
+
+    hospitalization
+
+    recovery
+
+*/
