978fbdb1c0b226bc0ca22d6609288c56d71f39dc
Tony Josi <tonyjosinew@gmail.com>
2020-05-07 23:15:17 +0530

    wip: pseudo for detailed flow

--- src/cv19ssim_core.c
+++ src/cv19ssim_core.c
@@ -3,13 +3,25 @@
 // main driver
 /*
 
+new data struct.:
+    is_hospitalized?
+    normal death rate
+    normal birth rate
+
 while(1)
 
     new deaths
+        death for non infected persons
+        loop through non covid & recvered 
+            death is functions of global death rate, immunity & random selection
 
     covid deaths
+        loop through covid infected
+            function of immunity and whether hospitalised or not and randomness
 
     new births
+        should be less than max. popl. limit
+        based on randomness and birth rate
 
     movement
 
