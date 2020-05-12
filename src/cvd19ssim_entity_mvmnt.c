9a7f9c7a35cc7a98fc27b4021bbe6bdad6b3c068
Tony Josi <tonyjosinew@gmail.com>
2020-05-13 00:31:24 +0530

    distance func added

--- src/cvd19ssim_entity_mvmnt.c
+++ src/cvd19ssim_entity_mvmnt.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <time.h>
+#include <math.h>
 #include <stdlib.h>
 #include "../inc/cvd19ssim_core.h"
 #include "../inc/cvd19ssim_ppm.h"
 #include "../inc/cvd19ssim_core_data_defs.h"
+
+#define SQUARE(X)                   (X * X)
+
+
+double pos_calc_distance(position_t pt1, position_t pt2) {
+    return (sqrt(SQUARE((pt1.x - pt2.x)) + SQUARE((pt1.y - pt2.y))));
+}
