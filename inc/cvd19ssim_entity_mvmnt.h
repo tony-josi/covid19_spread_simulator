a1cf49dc63d81b7cfb6afe6fa7270a5a3f110fb7
Tony Josi <tonyjosinew@gmail.com>
2020-05-13 10:43:50 +0530

    changes

--- inc/cvd19ssim_entity_mvmnt.h
+++ inc/cvd19ssim_entity_mvmnt.h
@@ -2,6 +2,8 @@
 #define _CVD19SSIM_ENTITY_MVMNT_H
 
 #include <stdint.h>
+#include <stdbool.h>
+#include "cvd19ssim_core.h"
 
 typedef struct _position {
     uint32_t x;
@@ -21,5 +23,7 @@ typedef struct _cvd19ssim_entity_mvmnt {
 } cvd19ssim_entity_mvmnt_t;
 
 double pos_calc_distance(position_t, position_t);
+bool check_if_in_spread_range(cvd19ssim_core_t hC, uint32_t i, uint32_t j);
+
 
 #endif /* _CVD19SSIM_ENTITY_MVMNT_H */
--- src/cvd19ssim_entity_mvmnt.c
+++ src/cvd19ssim_entity_mvmnt.c
@@ -1,14 +1,22 @@
-#include <stdio.h>
-#include <time.h>
 #include <math.h>
-#include <stdlib.h>
 #include "../inc/cvd19ssim_core.h"
-#include "../inc/cvd19ssim_ppm.h"
-#include "../inc/cvd19ssim_core_data_defs.h"
+#include "../inc/cvd19ssim_entity_mvmnt.h"
 
 #define SQUARE(X)                   (X * X)
 
 
 double pos_calc_distance(position_t pt1, position_t pt2) {
-    return (sqrt(SQUARE((pt1.x - pt2.x)) + SQUARE((pt1.y - pt2.y))));
+    
+    return (sqrt(MAG((SQUARE((pt1.x - pt2.x)) + SQUARE((pt1.y - pt2.y))))));
+
+}
+
+bool check_if_in_spread_range(cvd19ssim_core_t hCVD19, uint32_t i, uint32_t j) {
+    
+    if(pos_calc_distance(hCVD19.entities[i].pos_data.cur_pos, hCVD19.entities[j].pos_data.cur_pos) <= \
+    ((double)hCVD19.max_spread_distance))
+        return 1;
+    else
+        return 0;
+
 }
