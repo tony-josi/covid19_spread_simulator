d4de04a25400f2b247212baf88847f26749bf069
Tony Josi <tonyjosinew@gmail.com>
2020-05-18 22:45:05 +0530

    added private defs

--- inc/cvd19ssim_core_data_defs.h
+++ inc/cvd19ssim_core_data_defs.h
@@ -1,6 +1,8 @@
 #ifndef _CVD19SSIM_CORE_DATA_DEFS_H
 #define _CVD19SSIM_CORE_DATA_DEFS_H
 
+#include "cvdssim_core_private_defs.h"
+
 #define MAX_ALLOWED_POPULATION                  500
 
 #define INIT_POPL_LOWER_LIMIT_DIFF              400
@@ -11,27 +13,9 @@
 
 #define MAX_NUM_OF_LOOPS_TO_LOG                 250
 
-#define LOG_FILE_LINE_BUFF_SIZE                 150
-
-#define LOG_FILE_PATH                           "cvd19.log"
-
-#define MAX_SPREAD_DISTANCE                     10
-
-#define PERCENT_CHANCE_OF_CVD_INF_IN_SPRD_DIST  60
-
 #define MAX_SPEED                               2
 
 #define MIN_SPEED                               1
-
-#define CVD_DEATH_FACTOR                        20
-
-#define MAX_PROB_OF_EARLY_NORMAL_DEATH          60
-
-#define MIN_PROB_OF_EARLY_NORMAL_DEATH          10
-
-#define MAX_PROB_OF_BETTER_IMMUNITY             70
-
-#define MIN_PROB_OF_BETTER_IMMUNITY             30
 
 #define MIN_INITIALLY_INFECTED                  10
 
@@ -39,19 +23,9 @@
 
 #define DEFAULT_SLEEP_TIME                      10
 
-#define STILL_FRAMES_AT_START                   10
-
 #define PERCENT_OF_HIGH_SPEED                   10
 
 #define HIGH_SPEED_POSSIBLE                     10
-
-#define PERCENT                                 100
-
-#define PERCENT_CHANCE_BIRTHS_OCCUR             5
-
-#define PERCENT_CHANCE_DEATHS_OCCUR             5
-
-#define INVERSE_DEATH_CONSTANT                  20
 
 #define INF_MIN_DEATH_DAYS                      21
 
